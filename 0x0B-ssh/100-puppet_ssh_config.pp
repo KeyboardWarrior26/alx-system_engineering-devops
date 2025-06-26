147e53715dff41b50ecb8df2fd34a17f8520fb9f
Thamsanqa <thamzafaniso@gmail.com>
2025-06-26 22:13:24 +0200

    Add Puppet manifest to configure SSH client (Task 4)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,21 @@
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => '  IdentityFile ~/.ssh/school',
-  match => '^\s*IdentityFile\s+',
+# Ensure the SSH config file exists
+file { '/home/ubuntu/.ssh/config':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
 }
 
+# Add line to use private key
+file_line { 'Declare identity file':
+  path  => '/home/ubuntu/.ssh/config',
+  line  => '  IdentityFile ~/.ssh/school',
+  match => '^ *IdentityFile',
+}
+
+# Add line to disable password authentication
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
+  path  => '/home/ubuntu/.ssh/config',
   line  => '  PasswordAuthentication no',
-  match => '^\s*PasswordAuthentication\s+',
+  match => '^ *PasswordAuthentication',
 }
